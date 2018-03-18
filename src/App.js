7b19f979c412b766bec86a32c286e1695e8cddb7
jcgaza <jcgaza@up.edu.ph>
2018-03-18 16:47:43 +0800

    Test for fixed navbar, course card, header div

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import './App.css';
 import { Router } from 'react-router';
 import { createBrowserHistory } from 'history';
 import Routes from './pages/Routes';
+import Navbar from './pages/TestingFolder/Navbar';
+import Heading from './pages/TestingFolder/Heading';
+import Course from './pages/TestingFolder/Course';
 
 const browserHistory = createBrowserHistory();
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       	<Router history={browserHistory}>
-      		<Routes />
+      		<div>
+	      		<Navbar />
+	      		<Heading />
+	      		<Course />
+      		</div>
       	</Router>
       </div>
     );
---
+++ src/pages/TestingFolder/Heading.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Container, Header } from 'semantic-ui-react';
+import './Heading.css';
+
+class Heading extends Component {
+	render() {
+		return (
+			<div className="heading-main">
+			
+			</div>
+		);
+	}
+}
+
+export default Heading;
