b8561dc0dbec9b3b231aeb245ba9a02b40f20905
KimEzekiel <kldelmundo@up.edu.ph>
2018-04-14 01:49:51 +0800

    real time querying in admin/mange/user

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -2,70 +2,78 @@ import React, { Component } from 'react';
 import {Table, Grid} from 'semantic-ui-react';
 import autobind from 'react-autobind';
 
-const tableData = [
-  { name: 'John', age: 15, gender: 'Male' },
-  { name: 'Amber', age: 40, gender: 'Female' },
-  { name: 'Leslie', age: 25, gender: 'Female' },
-  { name: 'Ben', age: 70, gender: 'Male' },
-]
 
 
-class User extends Component {
-  constructor(){
+class UsersTable extends Component {
+  constructor(props){
     super(props);
     this.state = {
      column: null,
-     data: tableData,
-     direction: null
+     data: [],
+     direction: null,
+     visibleData: this.props.data
    }
     autobind(this);
   }
   //handle table sorting
-  handleSort = clickedColumn => () => {
-   const { column, data, direction } = this.state
-   if (column !== clickedColumn) {
-     this.setState({
-       column: clickedColumn,
-       data: _.sortBy(data, [clickedColumn]),
-       direction: 'ascending',
-     })
-     return
-   }
-
-   this.setState({
-     data: data.reverse(),
-     direction: direction === 'ascending' ? 'descending' : 'ascending',
-   })
-  }
+  // handleSort = clickedColumn => () => {
+  //  const { column, data, direction } = this.state
+  //  if (column !== clickedColumn) {
+  //    this.setState({
+  //      column: clickedColumn,
+  //      data: _.sortBy(data, [clickedColumn]),
+  //      direction: 'ascending',
+  //    })
+  //    return
+  //  }
+  //
+  //  this.setState({
+  //    data: data.reverse(),
+  //    direction: direction === 'ascending' ? 'descending' : 'ascending',
+  //  })
+  // }
 
   render() {
     return(
       <Table sortable celled fixed>
-             <Table.Header>
-               <Table.Row>
-                 <Table.HeaderCell sorted={column === 'name' ? direction : null} onClick={this.handleSort('name')}>
-                   Name
-                 </Table.HeaderCell>
-                 <Table.HeaderCell sorted={column === 'age' ? direction : null} onClick={this.handleSort('age')}>
-                   Age
-                 </Table.HeaderCell>
-                 <Table.HeaderCell sorted={column === 'gender' ? direction : null} onClick={this.handleSort('gender')}>
-                   Gender
-                 </Table.HeaderCell>
-               </Table.Row>
-             </Table.Header>
-             <Table.Body>
-               {_.map(data, ({ age, gender, name }) => (
-                 <Table.Row key={name}>
-                   <Table.Cell>{name}</Table.Cell>
-                   <Table.Cell>{age}</Table.Cell>
-                   <Table.Cell>{gender}</Table.Cell>
-                 </Table.Row>
-               ))}
-             </Table.Body>
-           </Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>
+              Name
+            </Table.HeaderCell>
+            <Table.HeaderCell>
+              Email
+            </Table.HeaderCell>
+            <Table.HeaderCell>
+              Status
+            </Table.HeaderCell>
+            <Table.HeaderCell>
+              Actions
+            </Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {
+            this.props.data.map( (user) =>{
+              return (
+                <Table.Row>
+                  <Table.Cell>
+                    {user.name}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {user.email}
+                  </Table.Cell>
+                  <Table.Cell>
+                    {user.status}
+                  </Table.Cell>
+                </Table.Row>);
+            })
+          }
+        </Table.Body>
+
+      </Table>
     );
   }
 }
 
-export default User;
+export default UsersTable;
