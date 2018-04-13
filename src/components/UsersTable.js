d2b1fe0cff23f63268b18e00e39c2f73e8058535
KimEzekiel <kldelmundo@up.edu.ph>
2018-04-13 23:38:16 +0800

    sorted table template'

--- src/components/UsersTable.js
+++ src/components/UsersTable.js
@@ -1,27 +1,69 @@
 import React, { Component } from 'react';
 import {Table, Grid} from 'semantic-ui-react';
-import CourseRow from './CourseRow';
 import autobind from 'react-autobind';
+
+const tableData = [
+  { name: 'John', age: 15, gender: 'Male' },
+  { name: 'Amber', age: 40, gender: 'Female' },
+  { name: 'Leslie', age: 25, gender: 'Female' },
+  { name: 'Ben', age: 70, gender: 'Male' },
+]
 
 
 class User extends Component {
   constructor(){
     super(props);
     this.state = {
-
-    }
+     column: null,
+     data: tableData,
+     direction: null
+   }
     autobind(this);
   }
+  //handle table sorting
+  handleSort = clickedColumn => () => {
+   const { column, data, direction } = this.state
+   if (column !== clickedColumn) {
+     this.setState({
+       column: clickedColumn,
+       data: _.sortBy(data, [clickedColumn]),
+       direction: 'ascending',
+     })
+     return
+   }
+
+   this.setState({
+     data: data.reverse(),
+     direction: direction === 'ascending' ? 'descending' : 'ascending',
+   })
+  }
+
   render() {
     return(
-      <Grid>
-        <Grid.Row>
-          add new users here
-        </Grid.Row>
-        <Grid.Row>
-
-        </Grid.Row>
-      </Grid>
+      <Table sortable celled fixed>
+             <Table.Header>
+               <Table.Row>
+                 <Table.HeaderCell sorted={column === 'name' ? direction : null} onClick={this.handleSort('name')}>
+                   Name
+                 </Table.HeaderCell>
+                 <Table.HeaderCell sorted={column === 'age' ? direction : null} onClick={this.handleSort('age')}>
+                   Age
+                 </Table.HeaderCell>
+                 <Table.HeaderCell sorted={column === 'gender' ? direction : null} onClick={this.handleSort('gender')}>
+                   Gender
+                 </Table.HeaderCell>
+               </Table.Row>
+             </Table.Header>
+             <Table.Body>
+               {_.map(data, ({ age, gender, name }) => (
+                 <Table.Row key={name}>
+                   <Table.Cell>{name}</Table.Cell>
+                   <Table.Cell>{age}</Table.Cell>
+                   <Table.Cell>{gender}</Table.Cell>
+                 </Table.Row>
+               ))}
+             </Table.Body>
+           </Table>
     );
   }
 }
--- src/pages/admin/User.js
+++ src/pages/admin/User.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Table, Grid} from 'semantic-ui-react';
 import CourseRow from './CourseRow';
+import UsersTable from '../components/UsersTable';
 import autobind from 'react-autobind';
 
 class User extends Component {
@@ -11,7 +12,7 @@ class User extends Component {
           add new users here
         </Grid.Row>
         <Grid.Row>
-        
+          <UsersTable/>
         </Grid.Row>
       </Grid>
     );
