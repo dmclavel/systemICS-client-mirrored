9110d42ae3b74487c7e064387d31aa46064aebae
Peter John Castillo <pgcastillo@up.edu.ph>
2018-04-16 18:58:28 +0800

    Done search

--- src/pages/faculty/Faculty.js
+++ src/pages/faculty/Faculty.js
@@ -4,10 +4,10 @@ import './Faculty.css';
 import SubjectCard from './SubjectCard';
 import Advisee from './Advisee';
 import socketIOClient from 'socket.io-client';
-import SearchAdvisee from './SearchAdvisee';
-import SearchCourse from './SearchCourse';
 import NavbarIn from '../components/navbar/NavbarIn';
 import DashboardHeader from '../components/headers/DashboardHeader';
+import SearchCard from '../../components/SearchCard';
+
 
 class Faculty extends Component {
    constructor(props){
@@ -16,28 +16,62 @@ class Faculty extends Component {
 	        endpoint: 'https://sleepy-falls-95372.herokuapp.com/',// the address of the server
           courses: [],
           advisees: [],
-          searchInput: ''
+          visibleCourses: [],
+          visibleAdvisees: [],
+          email_add: 'nuzumaki@konoha.edu.lof'
       }
  	 }
 
    componentDidMount = () =>{
+        console.log("componentDidMount");
 		    const socket = socketIOClient(this.state.endpoint); //establish connection to the server
 		    // listens on an endpoint and executes fallback function
-		    socket.emit('view_all_active_faculty_teaching_load', 'gpas@asdf.com');//send data to 'login' endpoint in server
-		    socket.on('view_all_active_faculty_teaching_load', (returnValueFromServer) => {
-		      console.log(returnValueFromServer);
-          this.setState({courses: returnValueFromServer})
+        console.log("START");
+		    socket.emit('search_assigned_sections_via_email', {email_add: this.state.email_add});//send data to 'login' endpoint in server
+        socket.on('search_assigned_sections_via_email', (returnValueFromServer) => {
+          this.setState({courses: returnValueFromServer, visibleCourses: returnValueFromServer})
 		    });
         socket.emit('view_adviser_advisee_information_all', 'gpas@asdf.com');//send data to 'login' endpoint in server
 		    socket.on('view_adviser_advisee_information_all', (returnValueFromServer) => {
 		      console.log(returnValueFromServer);
-          this.setState({advisees: returnValueFromServer})
+          this.setState({advisees: returnValueFromServer, visibleAdvisees: returnValueFromServer})
 		    });
 		}
-    handleCourseClick(e){
-        this.setState({ searchInput: e.target.value });
+    handleCourseSearch = (query) => {
+  		if(query.length == 0){
+        this.setState({visibleCourses: this.state.courses})
+        console.log(this.state.visibleCourses);
+  		}else{
+        this.setState(
+          {visibleCourses: this.state.courses.filter( (user) =>{
+              if( user.course_name.toLowerCase().includes(query.toLowerCase())){
+                console.log(this.state.visibleCourses);
+                return true;
+              }else{
+                return false;
+              }
+            })
+          }
+        )
+  		}
     }
-
+    handleAdviseeSearch = (query) => {
+  		if(query.length == 0){
+        this.setState({visibleAdvisees: this.state.advisees})
+        console.log(this.state.visibleAdvisees);
+  		}else{
+        this.setState(
+          {visibleAdvisees: this.state.advisees.filter( (user) =>{
+              if( user.student_name.toLowerCase().includes(query.toLowerCase()) || user.email_add.toLowerCase().includes(query.toLowerCase())){
+                return true;
+              }else{
+                return false;
+              }
+            })
+          }
+        )
+  		}
+  	}
 
   render() {
     return(
@@ -52,10 +86,10 @@ class Faculty extends Component {
                   <Grid.Column width={1}></Grid.Column>
                   <Grid.Column width={8}>
                      <Grid.Row>
-                       <Input fluid action={ <Button icon='search' onClick={ this.handleCourseClick } /> } placeholder='Search...' />
-                       {this.state.courses.map ((course)=>{
-                           return <SubjectCard name={course.course_name} title={course.course_title} description={course.description} section={course.section} no_of_students={course.no_of_students} capacity={course.max_capacity} room={course.room} time_start={course.time_start} time_end={course.time_end}/>
-                         })
+                       <SearchCard fluid={true} handleSearch={this.handleCourseSearch} placeholder="course name, course title, or section"/>
+                       {this.state.visibleCourses.map ((course)=>{
+                          return <SubjectCard name={course.course_name} title={course.course_title} description={course.description} section={course.section} no_of_students={course.no_of_students} capacity={course.max_capacity} room={course.room} time_start={course.time_start} time_end={course.time_end}/>
+                        })
                        }
                      </Grid.Row>
                   </Grid.Column>
@@ -65,8 +99,8 @@ class Faculty extends Component {
                     <Card fluid raised={true}>
                        <h2>Advisees</h2>
                     </Card>
-                    <Input fluid action={ <Button icon='search' onClick={ this.handleClick } /> } placeholder='Search...' />
-                      {this.state.advisees.map ((advisees)=>{
+                    <SearchCard fluid={true} handleSearch={this.handleAdviseeSearch} placeholder="name, email or student id"/>
+                      {this.state.visibleAdvisees.map ((advisees)=>{
                           return <Advisee name={advisees.student_name} student_number={advisees.student_number} curriculum={advisees.curriculum} email={advisees.email_add}/>
                         })
                       }
