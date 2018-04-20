8bf4f5f0e56925a152f41673061112bd5384be1f
amlagazon <amlagazon@up.edu.ph>
2018-04-20 14:54:08 +0800

    dropdown disabled if there's pending

--- src/pages/advisees/Advisees.js
+++ src/pages/advisees/Advisees.js
@@ -8,30 +8,24 @@ import Advisee from './Advisee';
 class Advisees extends Component {
       render() {
             return (
-                  <div className="Advisees">
-                        <Grid>
-                              <Grid.Row>
-                                    <NavbarIn
-                                          user={this.props.user}
-                                          active="manage"
-                                    />
-                                    <ManageHeader
-                                          accessLvl={this.props.accessLvl}
-                                          user={this.props.user}
-                                    />
-                              </Grid.Row>
-                              <Grid.Row>
-                                    <Grid.Column width={1} />
-                                    <Grid.Column width={4}>
-                                          <Sidebar />
-                                    </Grid.Column>
-                                    <Grid.Column width={10}>
-                                          <Advisee />
-                                    </Grid.Column>
-                              </Grid.Row>
-                        </Grid>
-                  </div>
-            );
+                  <div className='Advisees'>
+            <Grid>
+                  <Grid.Row>
+                        <NavbarIn user={this.props.user} active='manage' />
+                        <ManageHeader user={this.props.user} />
+                  </Grid.Row>
+                  <Grid.Row>
+                        <Grid.Column width={1} />
+                        <Grid.Column width={4}>
+                              <Sidebar />
+                        </Grid.Column>
+                        <Grid.Column width={10}>
+                              <Advisee />
+                        </Grid.Column>
+                  </Grid.Row>
+                  </Grid>
+      </div>
+            )     
       }
 }
 
