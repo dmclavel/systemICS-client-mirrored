b647300d780510429096344f6576752a0ab20785
jcgaza <jcgaza@up.edu.ph>
2018-04-22 20:29:03 +0800

    Fixed sidebar.

--- src/pages/components/Sidebar.js
+++ src/pages/components/Sidebar.js
@@ -146,11 +146,8 @@ class Sidebar extends Component {
 	}
 
 	handleOnChange = (e, data) => {
-<<<<<<< HEAD
 		this.props.handleChangeSemester(data.value.acad_year, data.value.semester);
-=======
 		console.log(data.value);
->>>>>>> master
 	};
 
 	render() {
@@ -195,11 +192,8 @@ class Sidebar extends Component {
 												</div>
 											}
 											onChange={this.handleOnChange}
-<<<<<<< HEAD
 											value={semesters[semesters.length - 1]}
 											text={`Change Semester`}
-=======
->>>>>>> master
 										/>
 									)}
 								</span>
