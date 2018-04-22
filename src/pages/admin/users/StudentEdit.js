dadd845fe8d333022fbb3d917312dd7a0eda9d4a
Ciara Mae Gotis <crgotis@up.edu.ph>
2018-04-22 12:28:44 +0800

    input sanitation in StudentEdit.js

--- src/pages/admin/users/StudentEdit.js
+++ src/pages/admin/users/StudentEdit.js
@@ -22,35 +22,64 @@ class StudentEdit extends Component {
       status: this.props.status,
       student_number: this.props.student_number,
       modalOpen: false,
-      address: 'https://sleepy-falls-95372.herokuapp.com/'
+      address: 'https://sleepy-falls-95372.herokuapp.com/',
+      isErrorName: false,
+      isErrorMail: false,
+      isErrorCurriculum: false,
+      isErrorStatus: false,
     };
     autobind(this);
   }
 
   handleName = (e) => {
+    if (e.target.value != ""){
+      this.setState({isErrorName: false});
+    }else{
+      this.setState({isErrorName: true});
+    }
     this.setState({name: e.target.value});
+    
   }
 
   handleEmail = (e) => {
+    if (e.target.value != ""){
+      this.setState({isErrorMail: false});
+    }else{  
+      this.setState({isErrorMail: true});
+    }
     this.setState({email_add: e.target.value});
   }
 
   handleCurriculum = (e) => {
+    if (e.target.value != ""){
+      this.setState({isErrorCurriculum: false});
+    }else{
+      this.setState({isErrorCurriculum: true});
+    }
     this.setState({curriculum: e.target.value});
   }
 
   handleStatus = (e) => {
+    if (e.target.value != ""){
+      this.setState({isErrorStatus: false});
+    }else{
+      this.setState({isErrorStatus: true});
+    }
     this.setState({status: e.target.value});
   }
 
   handleSubmit = (e) => {
-    const socket = socketIOClient(this.state.address); //establish connection to the server
-    socket.emit('modify_student', {student_number: this.state.student_number, name: this.state.name, email_add: this.state.email_add, status: this.state.status, curriculum: this.state.curriculum}); //send data to 'login' endpoint in server
-    socket.on('modify_student', returnValueFromServer => {
-      console.log(returnValueFromServer);
-    });
-    this.props.fetchData();
-    this.handleClose();
+    if (this.state.isErrorStatus == false && this.state.isErrorCurriculum == false && this.state.isErrorMail == false && this.state.isErrorName == false){
+      const socket = socketIOClient(this.state.address); //establish connection to the server
+      socket.emit('modify_student', {student_number: this.state.student_number, name: this.state.name, email_add: this.state.email_add, status: this.state.status, curriculum: this.state.curriculum}); //send data to 'login' endpoint in server
+      socket.on('modify_student', returnValueFromServer => {
+        console.log(returnValueFromServer);
+      });
+      this.props.fetchData();
+      this.handleClose();
+    }else{
+      alert("Please fill up all fields");
+    }
   }
 
   handleClose = (e) => {
@@ -68,12 +97,12 @@ class StudentEdit extends Component {
                 <Segment padded="very">
                     <Form>
                       <Form.Group widths='equal'>
-                        <Form.Input fluid label='Name' placeholder={this.state.name} onChange={this.handleName}/>
-                        <Form.Input fluid label='Email address' placeholder={this.state.email_add} onChange={this.handleEmail}/>
+                        <Form.Input error={this.state.isErrorName} fluid label='Name' placeholder={this.state.name} onChange={this.handleName}/>
+                        <Form.Input error={this.state.isErrorMail} fluid label='Email address' placeholder={this.state.email_add} onChange={this.handleEmail}/>
                       </Form.Group>
                       <Form.Group widths='equal'>
-                        <Form.Input fluid label='Curriculum' placeholder={this.state.curriculum} onChange={this.handleCurriculum}/>
-                        <Form.Input fluid label='Status' placeholder={this.state.status} onChange={this.handleStatus}/>
+                        <Form.Input error={this.state.isErrorCurriculum} fluid label='Curriculum' placeholder={this.state.curriculum} onChange={this.handleCurriculum}/>
+                        <Form.Input error={this.state.isErrorStatus} fluid label='Status' placeholder={this.state.status} onChange={this.handleStatus}/>
                       </Form.Group>
                     </Form>
                     <h2>
