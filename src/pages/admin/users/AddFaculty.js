f5b4a5f63f3a3fdfaddcb4f036ef2648623814f3
Ciara Mae Gotis <crgotis@up.edu.ph>
2018-04-22 13:43:36 +0800

    input sanitation in add faculty

--- src/pages/admin/users/AddFaculty.js
+++ src/pages/admin/users/AddFaculty.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Button, Modal, Form, Grid, Segment, Header, Dropdown, Container, Checkbox , Popup} from 'semantic-ui-react';
 import socketIOClient from 'socket.io-client';
 import autobind from 'react-autobind';
+import ErrorMessage from './ErrorMessage';
 
 const inlineStyle={
   modal :{
@@ -21,6 +22,12 @@ class AddFaculty extends Component {
       isRegCom: '',
       status: '',
       emp_no: '',
+      isErrorMessage: false,
+      isErrorName: false,
+      isErrorMail: false,
+      isErrorReg: false,
+      isErrorStatus: false,
+      isErrorNumber: false,
       modalOpen: false,
       options: [ { key: 'Faculty', value: 0, text: 'Faculty' }, { key: 'Admin', value: 2, text: 'Admin' }, { key: 'Registration Committee', value: 1, text: 'Registration Committee' }, ],
       address: 'https://sleepy-falls-95372.herokuapp.com/'
@@ -29,14 +36,29 @@ class AddFaculty extends Component {
   }
 
   handleName = (e) => {
+    if (e.target.value != ""){
+      this.setState({isErrorName: false});
+    }else{
+      this.setState({isErrorName: true});
+    }
     this.setState({name: e.target.value});
   }
 
   handleNumber = (e) => {
+    if (e.target.value != ""){
+      this.setState({isErrorNumber: false});
+    }else{
+      this.setState({isErrorNumber: true});
+    }
     this.setState({emp_no: e.target.value});
   }
 
   handleEmail = (e) => {
+    if (e.target.value != ""){
+      this.setState({isErrorMail: false});
+    }else{  
+      this.setState({isErrorMail: true});
+    }
     this.setState({email_add: e.target.value});
   }
 
@@ -45,17 +67,27 @@ class AddFaculty extends Component {
   }
 
   handleStatus = (e) => {
+    if (e.target.value != ""){
+      this.setState({isErrorStatus: false});
+    }else{
+      this.setState({isErrorStatus: true});
+    }
     this.setState({status: e.target.value});
   }
 
   handleSubmit = (e) => {
-    const socket = socketIOClient(this.state.address); //establish connection to the server
-    socket.emit('create_faculty', {emp_no: this.state.emp_no, name: this.state.name, email_add: this.state.email_add, status: this.state.status, isRegCom: this.state.isRegCom}); //send data to 'login' endpoint in server
-    socket.on('create_faculty', returnValueFromServer => {
-      console.log(returnValueFromServer);
-    });
-    this.props.fetchData();
-    this.handleClose();
+    if (this.state.isErrorStatus == false && this.state.isErrorMail == false && this.state.isErrorNumber == false && this.state.isErrorName == false && this.state.isErrorReg == false){
+      const socket = socketIOClient(this.state.address); //establish connection to the server
+      socket.emit('create_faculty', {emp_no: this.state.emp_no, name: this.state.name, email_add: this.state.email_add, status: this.state.status, isRegCom: this.state.isRegCom}); //send data to 'login' endpoint in server
+      socket.on('create_faculty', returnValueFromServer => {
+        console.log(returnValueFromServer);
+      });
+      this.props.fetchData();
+      this.handleClose();
+    }else{
+      this.setState({isErrorMessage: true});
+    }
+    
   }
 
   handleClose = (e) => {
@@ -70,21 +102,22 @@ class AddFaculty extends Component {
   render() {
     return(
        <Modal closeIcon size='large' style={inlineStyle.modal} trigger={<Button color="teal" onClick={this.handleOpen} open={this.state.modalOpen}
-        onOpen={this.handleOpen} onClose={this.handleClose}> Add Faculty </Button>} basic>
+        > Add Faculty </Button>} open={this.state.modalOpen} onOpen={this.handleOpen} onClose={this.handleClose} basic>
             <Modal.Content>
               <Container>
                 <Segment padded="very">
+                    { 
+                      this.state.isErrorMessage == true? <ErrorMessage/>: <div/>
+                    }
                     <Form>
-                      <Form.Group>
-                        <Form.Input fluid label='Employee Number' placeholder='Name' onChange={this.handleNumber}/>
+                      <Form.Group widths='equal'>
+                        <Form.Input error={this.state.isErrorName} fluid label='Name' placeholder='Name' onChange={this.handleName}/>
+                        <Form.Input error={this.state.isErrorMail} fluid label='Email address' placeholder='Email address' onChange={this.handleEmail}/>
                       </Form.Group>
                       <Form.Group widths='equal'>
-                        <Form.Input fluid label='Name' placeholder='Name' onChange={this.handleName}/>
-                        <Form.Input fluid label='Email address' placeholder='Email address' onChange={this.handleEmail}/>
-                      </Form.Group>
-                      <Form.Group widths='equal'>
-                        <Form.Dropdown fluid label = "Access Level" placeholder='Access Level' search selection options={this.state.options} onChange={this.handleAccessLevel}/>
-                        <Form.Input fluid label='Status' placeholder='Status' onChange={this.handleStatus}/>
+                        <Form.Dropdown error={this.state.isErrorReg} fluid label = "Access Level" placeholder='Faculty' search selection options={this.state.options} onChange={this.handleAccessLevel}/>
+                        <Form.Input error={this.state.isErrorStatus} fluid label='Status' placeholder='Status' onChange={this.handleStatus}/>
+                        <Form.Input error={this.state.isErrorNumber} fluid label='Employee Number' placeholder='Name' onChange={this.handleNumber}/>
                       </Form.Group>
                     </Form>
                     <h2>
